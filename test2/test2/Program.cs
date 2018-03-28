d5ae9b353d9cb258512e80d1ac9ea53c5825a544
LAPTOP-9RC7HJDL\陳柏霖 <aa02710@gmail.com>
2018-03-29 01:51:53 +0800

    雛形

--- test2/test2/Program.cs
+++ test2/test2/Program.cs
@@ -10,21 +10,37 @@ namespace test2
     {
         static void Main(string[] args)
         {
-            Random rand = new Random();
+            // 設定字的顏色
+            Console.ForegroundColor = ConsoleColor.White;
 
+            // 設定底色
+            Console.BackgroundColor = ConsoleColor.Red;
+
+            // 字串
+            Console.WriteLine("樂透號碼生成器");
+
+            // 迴圈 , 讓使用者可以重複使用
             while (true)
             {
-                int r1 = rand.Next(0, 43);
-                int r2 = rand.Next(0, 43);
-                int r3 = rand.Next(0, 43);
-                int r4 = rand.Next(0, 43);
-                int r5 = rand.Next(0, 43);
-                int r6 = rand.Next(0, 43);
-                int r7 = rand.Next(0, 43);
+                // 宣告Random變數ran
+                Random ran = new Random();
 
-                Console.WriteLine("號碼 : {0} {1} {2} {3} {4} {5} {6}", r1, r2, r3, r4, r5, r6, r7);
+                // 產生號碼 ， 範圍介於 1-42
+                int r1 = ran.Next(1, 43);
+                int r2 = ran.Next(1, 43);
+                int r3 = ran.Next(1, 43);
+                int r4 = ran.Next(1, 43);
+                int r5 = ran.Next(1, 43);
+                int r6 = ran.Next(1, 43);
+                
+                // 不重複
+                if (r1 != r2 && r1 != r3 && r1 != r4 && r1 != r5 && r1 != r6  && r2 != r3 && r2 != r4 && r2 != r5 && r2 != r6  && r3 != r4 && r3 != r5 && r3 != r6 &&  r4 != r5 && r5 != r6 )
+                {
+                    Console.WriteLine(" {0:00} {1:00} {2:00} {3:00} {4:00} {5:00}", r1, r2, r3, r4, r5, r6);
+                }
+                Console.ReadKey();
             }
-            Console.ReadLine();
         }
     }
 }
+
