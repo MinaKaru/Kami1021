ff41edf01e97a068052d215c00437dc5a05cc269
LAPTOP-9RC7HJDL\陳柏霖 <aa02710@gmail.com>
2018-03-29 03:58:13 +0800

    兌獎號那邊只完成一半 , 沒判定道中2個號也是沒中獎

--- test2/test2/Program.cs
+++ test2/test2/Program.cs
@@ -28,19 +28,19 @@ namespace test2
             while (true)
             {
 
-                // 產生號碼 ， 範圍介於 1-42
-                int r1 = ran.Next(1, 43);
-                int r2 = ran.Next(1, 43);
-                int r3 = ran.Next(1, 43);
-                int r4 = ran.Next(1, 43);
-                int r5 = ran.Next(1, 43);
-                int r6 = ran.Next(1, 43);
+                // 產生號碼 ， 範圍介於 1-49
+                int r1 = ran.Next(1, 49);
+                int r2 = ran.Next(1, 49);
+                int r3 = ran.Next(1, 49);
+                int r4 = ran.Next(1, 49);
+                int r5 = ran.Next(1, 49);
+                int r6 = ran.Next(1, 49);
 
                 // 不重複
-                if (r1 != r2 && r1 != r3 && r1 != r4 && r1 != r5 && r1 != r6  && r2 != r3 && r2 != r4 && r2 != r5 && r2 != r6  && r3 != r4 && r3 != r5 && r3 != r6 &&  r4 != r5 && r5 != r6 )
+                if (r1 != r2 && r1 != r3 && r1 != r4 && r1 != r5 && r1 != r6 && r2 != r3 && r2 != r4 && r2 != r5 && r2 != r6 && r3 != r4 && r3 != r5 && r3 != r6 && r4 != r5 && r5 != r6)
                 {
                     Console.WriteLine("");
-                    Console.WriteLine("您的第"+ i +"組號碼");
+                    Console.WriteLine("您的第" + i + "組號碼");
                     Console.WriteLine(" ╔═════════════════════════════╗ ");
                     Console.WriteLine(" ║ {0:00} ║ {1:00} ║ {2:00} ║ {3:00} ║ {4:00} ║ {5:00} ║", r1, r2, r3, r4, r5, r6);
                     Console.WriteLine(" ╚═════════════════════════════╝ ");
@@ -49,7 +49,7 @@ namespace test2
                 }
 
                 Console.WriteLine("輸入本期開獎號碼:");
-                
+
                 // 輸入號碼 
                 int b1 = int.Parse(Console.ReadLine());
                 int b2 = int.Parse(Console.ReadLine());
@@ -57,19 +57,23 @@ namespace test2
                 int b4 = int.Parse(Console.ReadLine());
                 int b5 = int.Parse(Console.ReadLine());
                 int b6 = int.Parse(Console.ReadLine());
-   
+
                 Console.WriteLine(" ╔═════════════════════════════╗ ");
                 Console.WriteLine(" ║ {0:00} ║ {1:00} ║ {2:00} ║ {3:00} ║ {4:00} ║ {5:00} ║ ", b1, b2, b3, b4, b5, b6);
                 Console.WriteLine(" ╚═════════════════════════════╝ ");
-                
+
                 // if 號碼都相等
                 if (r1 == b1 && r2 == b2 && r3 == b3 && r4 == b4 && r5 == b5 && r6 == b6)
                 {
                     Console.WriteLine("恭喜你中頭獎 !");
                 }
-                else
+                else if (r1 != b1 && r2 != b2 && r3 != b3 && r4 != b4 && r5 != b5 && r6 != b6)
                 {
-                    Console.WriteLine("哭哭哦 , 你沒中頭獎 !");
+                    Console.WriteLine("哭哭哦 , 你沒中獎 !");
+                }
+                else if (r1 == b1 && r2 != b2 && r3 != b3 && r4 != b4 && r5 != b5 && r6 != b6 || r1 != b1 && r2 == b2 && r3 != b3 && r4 != b4 && r5 != b5 && r6 != b6 || r1 != b1 && r2 != b2 && r3 == b3 && r4 != b4 && r5 != b5 && r6 != b6 || r1 != b1 && r2 != b2 && r3 != b3 && r4 == b4 && r5 != b5 && r6 != b6 || r1 != b1 && r2 != b2 && r3 != b3 && r4 != b4 && r5 == b5 && r6 != b6 || r1 != b1 && r2 != b2 && r3 != b3 && r4 != b4 && r5 != b5 && r6 == b6) 
+                {
+                    Console.WriteLine("哭哭哦 , 你沒中獎 !");
                 }
 
                 Console.ReadKey();
