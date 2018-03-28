4ec555e25d7ffedb42a7f15979251bbdf9e40984
LAPTOP-9RC7HJDL\陳柏霖 <aa02710@gmail.com>
2018-03-29 02:13:22 +0800

    美化+註解+優化界面

--- test2/test2/Program.cs
+++ test2/test2/Program.cs
@@ -11,19 +11,22 @@ namespace test2
         static void Main(string[] args)
         {
             // 設定字的顏色
-            Console.ForegroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Blue;
 
             // 設定底色
-            Console.BackgroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            // 宣告Random變數ran
+            Random ran = new Random();
 
             // 字串
             Console.WriteLine("樂透號碼生成器");
 
+            // 計算第幾組好碼;
+            int i = 1;
             // 迴圈 , 讓使用者可以重複使用
             while (true)
             {
-                // 宣告Random變數ran
-                Random ran = new Random();
 
                 // 產生號碼 ， 範圍介於 1-42
                 int r1 = ran.Next(1, 43);
@@ -32,12 +35,19 @@ namespace test2
                 int r4 = ran.Next(1, 43);
                 int r5 = ran.Next(1, 43);
                 int r6 = ran.Next(1, 43);
-                
+
                 // 不重複
                 if (r1 != r2 && r1 != r3 && r1 != r4 && r1 != r5 && r1 != r6  && r2 != r3 && r2 != r4 && r2 != r5 && r2 != r6  && r3 != r4 && r3 != r5 && r3 != r6 &&  r4 != r5 && r5 != r6 )
                 {
-                    Console.WriteLine(" {0:00} {1:00} {2:00} {3:00} {4:00} {5:00}", r1, r2, r3, r4, r5, r6);
+                    Console.WriteLine("");
+                    Console.WriteLine("您的第"+ i +"組號碼");
+                    Console.WriteLine(" ╔═════════════════════════════╗ ");
+                    Console.WriteLine(" ║ {0:00} ║ {1:00} ║ {2:00} ║ {3:00} ║ {4:00} ║ {5:00} ║", r1, r2, r3, r4, r5, r6);
+                    Console.WriteLine(" ╚═════════════════════════════╝ ");
+
+                    i = i + 1;
                 }
+  
                 Console.ReadKey();
             }
         }
